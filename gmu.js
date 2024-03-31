e9d526abb71f59328c362481fcc8452f6607fff3
“[firstname <[gmugsk@gmail.com]>
2024-03-31 11:39:27 +0500

    Assignment 09

---
+++ gmu.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Favorite Number: Store your favorite number in a variable.
+ Then, using that variable,
+ create a message that reveals your favorite number.
+  Print that message.*/
+let favuriteResult = 9;
+let messageResult = `my favourite ${favuriteResult}`;
+console.log(favuriteResult);
---
+++ gmu.ts
@@ -0,0 +1,8 @@
+/*Favorite Number: Store your favorite number in a variable.
+ Then, using that variable,
+ create a message that reveals your favorite number.
+  Print that message.*/
+
+let favuriteResult: number = 9;
+let messageResult: string = `my favourite ${favuriteResult}`;
+console.log(favuriteResult);
